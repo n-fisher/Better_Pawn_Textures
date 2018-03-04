4451d569d1e66b35bccef10131515b6f5b3d7dd5
fyarn <1361631+fyarn@users.noreply.github.com>
2018-03-03 21:22:28 -0800

    Add project files.

---
+++ Source/Better_Pawn_Textures.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Verse;
+using Harmony;
+
+namespace Better_Pawn_Textures
+{
+    public class Better_Pawn_Textures : Mod
+    {
+        public Better_Pawn_Textures(ModContentPack content) : base(content)
+        {
+            List<PawnKindDef> pawnKinds = ;
+
+            foreach (PawnKindDef kind in pawnKinds) {
+                foreach ;
+            }
+        }
+    }
+
+    public class PawnGeneratorPatch
+    {
+        HarmonyInstance harmony = HarmonyInstance.Create("com.fyarn.")
+    }
+}
